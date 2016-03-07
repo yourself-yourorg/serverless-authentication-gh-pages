b32f904d56be21ddac406360a77c8925c8ef5e63
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-03-07 09:33:50 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+$(function(){
+  $('.providers button').on('click', function(event){
+    var provider = $(event.target).attr('id');
+    window.location.href = 'https://i3tswjv8ak.execute-api.eu-west-1.amazonaws.com/dev/signin/' + provider;
+  });
+  var query = getQueryParams(document.location.search);
+  var token = query.token?query.token:'';
+  $('#token').html(token);
+});
+
+function getQueryParams(qs) {
+  qs = qs.split('+').join(' ');
+  var params = {},
+    tokens,
+    re = /[?&]?([^=]+)=([^&]*)/g;
+
+  while (tokens = re.exec(qs)) {
+    params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+  }
+  return params;
+}
+
