a0adbec87b10bd60aef2254e1bc903343bd9187e
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-03-08 16:05:30 +0200

    test token on load

--- app.js
+++ app.js
@@ -2,30 +2,34 @@
 
 var endpoint = 'https://i3tswjv8ak.execute-api.eu-west-1.amazonaws.com/dev';
 
-$(function(){
-  $('.providers button').on('click', function(event){
+$(function () {
+  $('.providers button').on('click', function (event) {
     var provider = $(event.target).attr('id');
     window.location.href = endpoint + '/signin/' + provider;
   });
   var query = getQueryParams(document.location.search);
-  var token = query.token?query.token:'';
+  var token = query.token ? query.token : '';
   $('#token').html(token);
   localStorage.setItem('token', token);
 
-  $('.testers #test').on('click', function() {
-     $.ajax({
-       method: 'GET',
-       url: endpoint + '/test-token',
-       headers: { Authorization: localStorage.getItem('token') }
-     })
-      .done(function(data) {
-        $('#test-result').html(JSON.stringify(data));
-      })
-      .fail(function(error) {
-        $('#test-result').html(JSON.stringify(error));
-      });
-  });
+  testToken();
+
+  $('.testers #test').on('click', testToken);
 });
+
+function testToken() {
+  $.ajax({
+      method: 'GET',
+      url: endpoint + '/test-token',
+      headers: {Authorization: localStorage.getItem('token')}
+    })
+    .done(function (data) {
+      $('#test-result').html(JSON.stringify(data));
+    })
+    .fail(function (error) {
+      $('#test-result').html('Unauthorized');
+    });
+}
 
 function getQueryParams(qs) {
   qs = qs.split('+').join(' ');
