33dba4fd8cf8c7839d3540d60e996eaa42407b93
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-04-23 23:47:39 +0300

    upgrade to 0.5.0 serverless boilerplate

--- app.js
+++ app.js
@@ -10,17 +10,57 @@ function testToken() {
       method: 'GET',
       url: endpoint + '/test-token',
       headers: {
-        Authorization: localStorage.getItem('token')
+        Authorization: localStorage.getItem('authorization_token')
       }
     })
     .done(function (data) {
       $('#test-result').html(JSON.stringify(data));
     })
     .fail(function (error) {
+      if($('#auto-refresh').prop('checked')) {
+        $('#test-result').html('Refreshing token...');
+        refreshToken();
+      } else {
+        $('#test-result').html('Unauthorized');
+      }
+    });
+}
+
+function refreshToken() {
+  $('#test-result').html('Loading...');
+
+  // refresh token
+  $.ajax({
+      method: 'GET',
+      url: endpoint + '/authentication/refresh?refresh_token=' + localStorage.getItem('refresh_token') + '&id=' + localStorage.getItem('id')
+    })
+    .done(function (data) {
+      console.log(data.errorMessage);
+      if (data.errorMessage) {
+        $('#test-result').html(data.errorMessage);
+      } else {
+        saveResponse(data.authorization_token, data.refresh_token, data.id);
+        testToken();
+      }
+    })
+    .fail(function (error) {
       $('#test-result').html('Unauthorized');
     });
 }
 
+function saveResponse(authorization_token, refresh_token, id) {
+  $('#token').html('authorization_token:'+authorization_token+'<hr>refresh_token:'+refresh_token+'<hr>id:'+id);
+  // Save token to local storage for later use
+  localStorage.setItem('authorization_token', authorization_token);
+  localStorage.setItem('refresh_token', refresh_token);
+  localStorage.setItem('id', id);
+}
+
+/*
+ headers: {
+  Authorization: localStorage.getItem('refresh')
+ }
+ */
 function getPathFromUrl(url) {
   return url.split(/[?#]/)[0];
 }
@@ -46,20 +86,19 @@ $(function () {
   });
 
   $('#logout').on('click', function(event) {
-    localStorage.removeItem('token');
+    localStorage.removeItem('authorization_token');
+    localStorage.removeItem('refresh_token');
+    localStorage.removeItem('id');
     window.location.href = getPathFromUrl(window.location.href);
   });
 
   var query = getQueryParams(document.location.search);
-  var token = query.token ? query.token : '';
-  $('#token').html(token);
-
-  // Save token to local storage for later use
-  localStorage.setItem('token', token);
+  saveResponse(query.authorization_token, query.refresh_token, query.id);
 
   // trigger test token
   testToken();
 
   $('.testers #test').on('click', testToken);
-});
+  $('.testers #refresh').on('click', refreshToken);
 
+});
